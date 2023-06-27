75ba0fc27bf53683c153e8fa791b107d1be45364
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-06-27 17:09:54 -0500

    feat: :sparkles: conexion mejorada
    reduciendo codigo

--- scripts/db/connect.php
+++ scripts/db/connect.php
@@ -10,6 +10,8 @@ class connect {
             //  se crea una nueva instancia de la clase PDO, que se utiliza para conectarse a la base de datos
             $this->con=new \PDO( $_ENV["DSN"].":host=".$_ENV["HOST"].";dbname=".$_ENV["DBNAME"].";user=". $_ENV["USERNAME"].";password=".$_ENV["PASSWORD"].";port=". $_ENV["PORT"]);
             //  Aquí se establece un atributo en la conexión PDO para habilitar el modo de excepción. Esto significa que PDO lanzará excepciones en caso de errores en las consultas SQL.
+            $this->con->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_SILENT);
+            $this->con->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_WARNING);
             $this->con->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 
             // print_r("conexion exitosa");
